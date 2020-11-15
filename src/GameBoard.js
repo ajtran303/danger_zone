771ff50fabd132a665824031de96c5b35f93e7dc
AJ Tran <ajtrandev@gmail.com>
2020-11-14 20:00:36 -0700

    Add feature: the board shows cells of categories and points
      Closes #5
      Closes #8
    
    Co-authored-by: Kwibe Merci <46441816+jKwibe@users.noreply.github.com>

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,8 +1,16 @@
-import "./GameBoard.css"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import CellColumn from "./CellColumn";
+import "./GameBoard.css";
 
 export default function GameBoard() {
+  const categories = ['1', '2', '3', '4', '5', '6']
+
   return (
-    <section className="game-board">
-    </section>
+    <Container className="game-board">
+      <Row>
+      { categories.map( (c) => <CellColumn category={`Category ${c}`}/> ) }
+      </Row>
+    </Container>
   )
 };
