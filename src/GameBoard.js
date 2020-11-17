a128cbbb23e53ac111b34b57ee1a1044a1d963c9
AJ Tran <ajtrandev@gmail.com>
2020-11-17 15:30:49 -0700

    Pass props from GameBoard to CellColumns for questions data

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -3,13 +3,19 @@ import Row from 'react-bootstrap/Row';
 import CellColumn from "./CellColumn";
 import "./GameBoard.css";
 
+import animals from './data/animals'
+import compsci from './data/compsci'
+import geography from './data/geography'
+import mythology from './data/mythology'
+import vehicles from './data/vehicles'
+
 export default function GameBoard() {
-  const categories = ['1', '2', '3', '4', '5', '6']
+  const categories = [animals, compsci, geography, mythology, vehicles]
 
   return (
     <Container className="game-board">
       <Row>
-      { categories.map( (c) => <CellColumn category={`Category ${c}`}/> ) }
+      { categories.map( (category) => <CellColumn category={category[0]['category']}  questions={category} /> ) }
       </Row>
     </Container>
   )
