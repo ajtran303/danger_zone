5d9e60db69b4052637b768635be28849bebb3cb4
AJ Tran <ajtrandev@gmail.com>
2020-11-17 15:57:46 -0700

    Pass only the question (string) to cell

--- src/Cell.js
+++ src/Cell.js
@@ -1,9 +1,15 @@
 import "./Cell.css"
 
 export default function Cell(props) {
+
+  // onClick, this component needs to:
+  // 1. hide the current contents of the gameboard
+  // 2. pass its question prop to a new component
+
   return (
     <section className="cell">
       {props.name}
+      {/* {props.question} */}
     </section>
   )
 };
--- src/CellColumn.js
+++ src/CellColumn.js
@@ -5,7 +5,7 @@ export default function CellColumn(props) {
   return (
     <Column className='cell-column'>
       <Cell name={props.category} />
-      { props.questions.map( (question, index) => <Cell name={(index+1)*100} question={question}/> ) }
+      { props.questions.map( (question, index) => <Cell name={(index+1)*100} question={question.question}/> ) }
     </Column>
-  )
+  ) 
 };
