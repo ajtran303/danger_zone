f5157875f57314f285a0cc3c04958b38fde895f9
AJ Tran <ajtrandev@gmail.com>
2020-11-18 14:03:57 -0700

    Refactor CellColumn with object destructuring

--- src/CellColumn.js
+++ src/CellColumn.js
@@ -1,11 +1,11 @@
 import Column from 'react-bootstrap/Col';
 import Cell from './Cell'
 
-export default function CellColumn(props) {
+export default function CellColumn({ category, questions }) {
   return (
     <Column className='cell-column'>
-      <Cell name={props.category} />
-      { props.questions.map( (question, index) => <Cell name={(index+1)*100} question={question.question}/> ) }
+      <Cell name={category} />
+      { questions.map( (question, index) => <Cell name={(index+1)*100} question={question.question} answer={question.correct_answer}/> ) }
     </Column>
   ) 
 };
