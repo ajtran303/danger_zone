849b903015375900c80fd00cf910af7ff889754d
AJ Tran <ajtrandev@gmail.com>
2020-11-18 14:03:14 -0700

    Update Cell positions when clicked

--- src/Cell.js
+++ src/Cell.js
@@ -2,21 +2,25 @@ import React, { useState } from 'react';
 import Question from './Question';
 import "./Cell.css"
 
-export default function Cell({ name, question }) {
+export default function Cell({ name, question, answer }) {
   const [active, setActive] = useState(false)
 
   const handleChange = (event) => {
       event.preventDefault();
       setActive(true);
+      // tell the Gameboard question, answer
+      // can we call Gameboard.setQuestionActive()
     }
 
   return (
     <div>
-      { !active && 
+      { 
+        !active && 
         <section className="cell" onClick={handleChange}>{name}</section>
       }
-      {
-        active && < Question question={question} />
+      { 
+        active && 
+        <section className="clicked-cell"></section>
       }
     </div>
   )
